7a0240754c7b406616a9f604e8fe608a04d4c2f6
akalewar-altimetrik <akashkalewar4@gmail.com>
2026-03-27 19:52:05 +0530

    Sliding window duplicates coding question.

---
+++ ContainsDuplicate.java
@@ -0,0 +1,23 @@
+//Time is O(n) as we loop through the array once → O(n).
+//Space is O(n) as there are no duplicates all elements will go in map.
+
+import java.util.*;
+public class ContainsDuplicate {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length; i++){
+            if (map.containsKey(nums[i])){
+                if (Math.abs(map.get(nums[i]) - i) <= k){
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
